a6bc1cbb5d359b3403c27c84b3d5e764cc6c3863
Dragonhv <himanshu.gsverma@gmail.com>
2024-03-23 19:07:05 +0530

    node server pages

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const http = require("http");
 
-const server = http.createServer((res, req) => {
-  console.log("Popcorn");
+const server = http.createServer((req, res) => {
+  // console.log(req);
+  const url = req.url;
+  if (url === "/home") {
+    res.write("<html>");
+    res.write("<head><title>hemlo</title></head>");
+    res.write("<body><h1>Welcome Home</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>hemlo</title></head>");
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/node") {
+    res.write("<html>");
+    res.write("<head><title>hemlo</title></head>");
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>hemlo</title></head>");
+  res.write("<body><h1>Welcome to my Nodejs Project</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(4000);
+server.listen(3000);
