e96f935f733052b502f9119b23c1968972a8cff8
Dragonhv <himanshu.gsverma@gmail.com>
2024-03-26 03:58:13 +0530

    added fs

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
 const http = require("http");
 const fs = require("fs");
+const { log } = require("console");
 
 const server = http.createServer((req, res) => {
   // console.log(req);
   const url = req.url;
-  if (url === "/home") {
+  const method = req.method;
+  const msg = fs.readFileSync("./message.txt");
+  if (url === "/") {
     res.write("<html>");
-    res.write("<head><title>hemlo</title></head>");
-    res.write("<body><h1>Welcome Home</h1></body>");
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/about") {
-    res.write("<html>");
-    res.write("<head><title>hemlo</title></head>");
-    res.write("<body><h1>Welcome to About Us page</h1></body>");
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/node") {
-    res.write("<html>");
-    res.write("<head><title>hemlo</title></head>");
-    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+
+    res.write("<head><title>hemlo form</title></head>");
+    res.write(msg);
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
     res.write("</html>");
     return res.end();
   }
 
+  // if (url === "/home") {
+  //   res.write("<html>");
+  //   res.write("<head><title>hemlo</title></head>");
+  //   res.write("<body><h1>Welcome Home</h1></body>");
+  //   res.write("</html>");
+  //   return res.end();
+  // }
+  // if (url === "/about") {
+  //   res.write("<html>");
+  //   res.write("<head><title>hemlo</title></head>");
+  //   res.write("<body><h1>Welcome to About Us page</h1></body>");
+  //   res.write("</html>");
+  //   return res.end();
+  // }
+  // if (url === "/node") {
+  //   res.write("<html>");
+  //   res.write("<head><title>hemlo</title></head>");
+  //   res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+  //   res.write("</html>");
+  //   return res.end();
+  // }
+
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", (chunk) => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      // res.setHeader("Location", "/");
+      return res.end();
+    });
   }
 
   res.setHeader("Content-Type", "text/html");
