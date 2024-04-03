6b35100c10adb56126a4612dcfd222169ac00f1b
Dragonhv <himanshu.gsverma@gmail.com>
2024-04-03 12:58:40 +0530

    added  routes

--- app.js
+++ app.js
@@ -7,19 +7,16 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type = "text" name="title"><input type = "text" name="size"><button type = "submit">Add Product</button> </form>'
-  );
-});
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>HELLO</h1>");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin' ,adminRoutes);
+app.use('/shop' ,shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
 });
 
 // const server = http.createServer(app);
